2132732528bd67ebef8f242a8ff893cd4ced31ed
芊里望月 <1374675587@qq.com>
2024-06-02 16:45:04 +0800

    修复了中文不完全显示的问题,信息内容支持中文

--- pkg/send_sms/send.go
+++ pkg/send_sms/send.go
@@ -2,16 +2,21 @@ package send_sms
 
 import (
 	"github.com/go-vgo/robotgo"
+	"github.com/go-vgo/robotgo/clipboard"
 	"time"
 )
 
 // AutoSendMessage 自动发送轰炸信息
 func AutoSendMessage(message string, x, y, n int) {
 	robotgo.MoveClick(x, y)
-	time.Sleep(2 * time.Second)
+	time.Sleep(1 * time.Second)
+	_ = clipboard.WriteAll(message)
 	for range n {
-		robotgo.TypeStr(message)
-		time.Sleep(time.Millisecond * 500)
+		_ = robotgo.KeyToggle(robotgo.Ctrl, "down")
+		_ = robotgo.KeyTap(robotgo.KeyV)
+		_ = robotgo.KeyToggle(robotgo.Ctrl, "up")
+		time.Sleep(10 * time.Millisecond)
 		_ = robotgo.KeyTap(robotgo.Enter)
 	}
+
 }
